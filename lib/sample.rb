201431e60ff1e3e2364be9e650d860ad69afac9a
Kazuki Yunoue <you.know.ue1480@gmail.com>
2011-12-27 12:32:13 +0900

    ) was unnecessary.

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,6 +1,6 @@
 module Dolittle
   def self.sample(*args)
-    puts "Dolittle has option(s), for example #{args.join(",")})."
+    puts "Dolittle has option(s), for example #{args.join(",")}."
     puts "Dolittle has env(s), for example EVN[\"RUBYLIB\"]:#{ENV["RUBYLIB"]})."
   end
 end
